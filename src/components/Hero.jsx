4bb48907d94596fc422ae3550bfd6821ac9aa0f6
Francesco Torchia <francescotorchia10@gmail.com>
2025-04-14 15:40:59 +0200

    fine

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
@@ -13,7 +14,7 @@ const Hero = () => {
           </div>
           <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">SURFBOARDS</h1>
           <div className="flex items-center gap-2 mt-3">
-            <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
+           <Link to='/surfboards' ><p className="font-semibold text-sm md:text-base">SHOP NOW</p></Link> 
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
           </div>
         </div>
